42915a06b22aa88c418679867e91180f24df46fb
D <shermadinovv@gmail.com>
2026-04-25 01:17:31 +0200

    Initial commit: Spring Boot project structure

---
+++ xantares/src/main/java/com/boogoo/xantares/XantaresApplication.java
@@ -0,0 +1,13 @@
+package com.boogoo.xantares;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XantaresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XantaresApplication.class, args);
+	}
+
+}
